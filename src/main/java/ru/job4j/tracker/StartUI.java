03a72c7fe788a9121ef40cd16d2fdb08869404eb
psychoton8 <arrow1564940milana@gmail.com>
2022-04-23 15:07:21 +0300

    6. Date. Отображение даты. [#310017 #291558]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,11 +4,14 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
-    private LocalDateTime created = LocalDateTime.now();
-    Item item = new Item();
-    LocalDateTime itemDateTime = item.getCreated();
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-    String currentDateTimeFormat = created.format(formatter);
+    public static void main(String[] args) {
+        LocalDateTime created = LocalDateTime.now();
+        Item item = new Item();
+        LocalDateTime itemDateTime = item.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = created.format(formatter);
+        System.out.println(currentDateTimeFormat);
+    }
 }
 
 
