ea76eb12cc6d88e5291d86917894771aebc8ed62
psychoton8 <arrow1564940milana@gmail.com>
2022-05-08 10:58:31 +0300

    9. toString [#310018 #297116]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -47,7 +47,7 @@ public class Item {
         return "Item{"
                 + "id=" + id
                 + ", name='" + name + '\''
-                + ", created=" + created
+                + ", created=" + created.format(FORMATTER)
                 + '}';
     }
 }
