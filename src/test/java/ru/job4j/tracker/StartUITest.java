96d3ad481e4aaeedaf55ead707f6bb1bb06bd5ab
psychoton8 <arrow1564940milana@gmail.com>
2022-08-10 14:38:26 +0300

    5.1. Тестирование. Подготовка данных. [#182960 #330220]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -15,4 +15,22 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
     }
+
+    @org.junit.jupiter.api.Test
+    void editItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "edited item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteItem() {
+    }
 }
