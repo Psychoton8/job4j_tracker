e18ab5e4703ede3c53678b0d11267960b18a3c8a
psychoton8 <arrow1564940milana@gmail.com>
2022-11-28 18:04:27 +0300

    0.4. Пользовательские исключения. [#219367 #365235]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -3,28 +3,25 @@ package ru.job4j.ex;
 import java.util.Objects;
 
 public class FindEl {
-    public static int indexOf(String[] value, String key) {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
             if (Objects.equals(value[i], key)) {
                 rsl = i;
+                break;
+            } else {
+                throw new ElementNotFoundException("Element wasn't found");
             }
         }
         return rsl;
     }
 
-   public static void load(String mess) throws ElementNotFoundException {
-        if (mess == null) {
-            throw new ElementNotFoundException("Element wasn't found");
-        }
-   }
-
     public static void main(String[] args) {
+        String[] dayOfWeek = new String[] {"Monday", "Tuesday", "Wednesday"};
         try {
-            load("wow");
+            indexOf(dayOfWeek, "Monday");
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
     }
-
 }
