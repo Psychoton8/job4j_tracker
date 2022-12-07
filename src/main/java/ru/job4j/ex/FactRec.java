378ecd9f595680e17c65875698b0dc1e19dc4fb0
psychoton8 <arrow1564940milana@gmail.com>
2022-12-07 14:33:09 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370]

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -8,6 +8,7 @@ public class FactRec {
             return calc(n - 1) * n;
         }
     }
+
     public static void main(String[] args) {
         int rsl = calc(3);
         System.out.println(rsl);
