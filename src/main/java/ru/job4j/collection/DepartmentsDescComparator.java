6c70e166dcaf56caedf1570b325b53e31d290cb2
psychoton8 <arrow1564940milana@gmail.com>
2024-02-23 16:14:36 +0100

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepartmentsDescComparator implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        if(!right.split("/")[0].equals(left.split("/")[0])) {
+            return right.split("/")[0].compareTo(left.split("/")[0]);
+        }
+        return left.compareTo(right);
+    }
+}
